bbaef460bc8f42eaeed2933e342bb317007a6999
developerjini <i5veagl@gmail.com>
2023-05-09 12:07:05 +0900

    뒷 정리하기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import Info from "./Info";
 
-const App = () => {
-  return <Info />;
-};
+function App() {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
+}
 
 export default App;
