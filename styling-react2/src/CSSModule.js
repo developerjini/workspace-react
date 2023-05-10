e0eeb063fb6fc6c771fd048ed60cdc8ace73580f
developerjini <i5veagl@gmail.com>
2023-05-10 10:13:01 +0900

    classnames

--- styling-react2/src/CSSModule.js
+++ styling-react2/src/CSSModule.js
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "./CSSModule.module.css";
+import classNames from "classnames/bind";
 
 const CSSModule = () => {
+  // active
+  // disabled, selected, checked
+  const cx = classNames.bind(styles);
+
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx("wrapper", "inverted")}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
