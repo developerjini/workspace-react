4676a1baef569a3caf98c8a4d84e92b1b81d78d8
developerjini <i5veagl@gmail.com>
2023-05-11 14:50:42 +0900

    뉴스 뷰어 UI 만들기 NewsItem 만들기 NewsList 만들기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,32 +1,7 @@
-import { useState } from 'react';
-import axios from '../node_modules/axios/index';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=f2449f41302b4309a6684a3c72ee6e0c',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
