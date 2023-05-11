6a942b5ae9497cc1ffa3d3f7649b7c27acbfaa1e
developerjini <i5veagl@gmail.com>
2023-05-11 11:07:54 +0900

    Outlet 컴포넌트 사용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
     </Routes>
   );
 }
