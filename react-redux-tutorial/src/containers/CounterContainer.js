08716fcdace97db5e97524a9b6277ed43fc069d3
developerjini <i5veagl@gmail.com>
2023-05-15 10:49:27 +0900

    Hooks를 사용하여 컨테이너 컴포넌트 만들기 useSelector로 상태조회하기 useDispatch를 사용하여 액션 디스패치하기

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,39 +1,56 @@
-import React from 'react';
-import Counter from '../components/Counter';
-import { connect } from 'react-redux';
-import { increase, decrease } from '../modules/counter';
+// import React from 'react';
+// import Counter from '../components/Counter';
+// import { connect } from 'react-redux';
+// import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+// // const default connect(
+// //   state => ({
+// //     number: state.counter.number
+// //   }),
+// //   dispatch => ({
+// //     increase:() => dispatch(increase()),
+// //     decrease:() => dispatch(decrease()),
+// //   }),
+// // )(CounterContainer);
+
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+
+// const mapDispatchToProps = { increase, decrease };
+
+// // bindActionCreators({increase, decrease }), dispatch);
+
+// // increase: () => {
+// //   dispatch(increase());
+// // },
+// // decrease: () => {
+// //   dispatch(decrease());
+// // },
+
+// // });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-// const default connect(
-//   state => ({
-//     number: state.counter.number
-//   }),
-//   dispatch => ({
-//     increase:() => dispatch(increase()),
-//     decrease:() => dispatch(decrease()),
-//   }),
-// )(CounterContainer);
-
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-
-const mapDispatchToProps = { increase, decrease };
-
-// bindActionCreators({increase, decrease }), dispatch);
-
-// increase: () => {
-//   dispatch(increase());
-// },
-// decrease: () => {
-//   dispatch(decrease());
-// },
-
-// });
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
